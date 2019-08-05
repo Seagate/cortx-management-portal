5c0548778ee6aac803590452a51484e59354f855
Piyush Gupte <piyush.gupte@seagate.com>
2019-08-05 14:58:44 +0530

    Description: GUI folder created. First check-in.

---
+++ gui/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app>
+    <v-app-bar flat dark app>
+      <v-toolbar-title class="headline ">
+        <span>Seagate-</span>
+        <span class="font-weight-light">EOS</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+        <span class="mr-2"></span>
+    </v-app-bar>
+
+    <v-content>
+     
+    </v-content>
+  </v-app>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'App',
+  components: {
+  },
+  data: () => ({
+    //
+  }),
+});
+</script>
