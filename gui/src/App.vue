7efd9391da433f7c44e0da5b23fc14b227eea031
Sanjeevan Bhave <sanjeevan.bhave@seagate.com>
2019-08-21 20:50:56 +0530

    EOS-1295: Medium Alert Mode
    - Created Basic Component structure to align/consume Navigation Panel and Routes
    - Created Navigation Panel with Basic Syle and Icon implementation
    - Created Basic Medium Alert Widget which will Show Title, System Health Componet placeholer and Alert Table as a Placeholder

--- gui/src/App.vue
+++ gui/src/App.vue
@@ -2,7 +2,14 @@
   <v-app>
     <headerBar />
     <div class="main-view">
-      <router-view></router-view>
+      <v-row>
+        <v-col cols=2>
+          <eos-nav-bar></eos-nav-bar>
+        </v-col>
+        <v-col cols=10>
+          <router-view></router-view>
+        </v-col>
+      </v-row>
     </div>
   </v-app>
 </template>
@@ -10,21 +17,21 @@
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import HeaderBar from "./components/header/HeaderBar.vue";
-
+import EosNavBar from "./components/navigation/NavBar.vue";
 @Component({
   name: "App",
   components: {
-    headerBar: HeaderBar
+    headerBar: HeaderBar,
+    eosNavBar: EosNavBar
   }
 })
 export default class App extends Vue {}
 </script>
 
 <style lang="scss" scoped>
-.main-view{
+.main-view {
   position: relative;
-  top:80px;
+  top: 60px;
 }
-
 </style>
 
--- gui/src/router.ts
+++ gui/src/router.ts
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
+import Dashboard from "./components/dashboard/Dashboard.vue";
 
 Vue.use(Router);
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: "/",
-      name: "home",
-      component: Home,
+      name: "dashboard",
+      component: Dashboard,
     },
   ],
 });
