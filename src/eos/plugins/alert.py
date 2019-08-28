c8d103d9b26955ff16fdfc7855037460fc0a9bd6
Pawan Lumar Srivastava <pawan.kumarsrivastava@seagate.com>
2019-08-28 19:30:04 +0000

    EOS-1440 Created CSM Schema for validation and converted SSPL JSON to CSM JSON

--- src/eos/plugins/alert.py
+++ src/eos/plugins/alert.py
@@ -21,7 +21,11 @@ from csm.common.errors import CsmError
 from csm.common.log import Log
 from csm.core.blogic import const
 import errno
-import json
+import json, os
+from jsonschema import Draft3Validator
+from jsonschema import validate
+from enum import Enum
+import datetime
 
 class AlertPlugin(CsmPlugin):
     """
@@ -35,6 +39,17 @@ class AlertPlugin(CsmPlugin):
         super().__init__()
         self.comm_client = AmqpComm()
         self.monitor_callback = None
+        try:
+            """ Validating the CSM Schema with Draft3Validator """
+            if os.path.isfile(const.CSM_HW_SCHEMA):
+                with open(const.CSM_HW_SCHEMA, 'r') as hw_file:
+                    self._schema = hw_file.read()
+            """ Remove tabs and newlines """
+            self._hw_schema = json.loads(' '.join(self._schema.split()))
+            """ Validate the schema """
+            Draft3Validator.check_schema(self._hw_schema)
+        except Exception as e:
+            Log.exception(e)
 
     def init(self, callback_fn):
         """
@@ -94,30 +109,95 @@ class AlertPlugin(CsmPlugin):
         """
         self.comm_client.stop()
 
+    def _prepare_common_details(self, body):
+        """
+        This method fetches the values from SSPL schema and then
+        creates a CSM schema containing common values i.e status, 
+        health, location etc.
+        """
+        data = {'id': int((body.get('enclosure-id', -1)))\
+                , 'alert_uuid': int((body.get('enclosure-id', -1))),\
+                'status': '%s' %(body.get('status', "")), 'type': 'hw',\
+                'enclosure_id': int(body.get('enclosure-id', -1)),\
+                'module_name': '%s' %((body.get\
+                ('name', ""))), 'description': '%s' %(body.get\
+                ('health-reason', "")), 'health': '%s' %(body.get\
+                ('health', "")), 'health_recommendation': '%s' %\
+                ((body.get('health-recommendation', ""))), 'location':\
+                '%s' %((body.get('location', ""))), 'resolved': 0,\
+                'acknowledged': 0, 'severity': 1}
+        return data
+
+    def _prepare_csm_schema(self, body, key):
+        """
+        This method first collects the common details and then creates
+        an extended info containg all the fields from extended_info and 
+        info dictionaries. 
+        """
+        data = {}
+        details = {}
+        try:
+            """
+            For Fan related alerts the common fiels resides under
+            info.fan_module. For all the other alerts it resides under
+            info.
+            """
+            if key == const.FAN_ALERT:
+                details = body[key]['info']['fan_module']
+            else:
+                details = body[key]['info']
+            """ Fetching the common values for CSM schema. """
+            data = self._prepare_common_details(details)
+            data.update({'state': '%s' %(body[key].get('alert_type', ""))})
+            """ Fetching values from extended_info. """
+            extended_info = body[key]['extended_info']
+            """
+            Creating extended_info for CSM schema. This extended_info will
+            contain all the remaining fields other then the common one. 
+            The fields for extended_info is fetched from extended_info and
+            info dictionaries of SSPL schema.
+            """
+            data['extended_info'] = {'resource_type': '%s'\
+                %(body[key].get('resource_type', ""))}
+            for info in extended_info:
+                data['extended_info'].update({'%s' %(info):\
+                        extended_info[info]})
+            """
+            Removing the already added fields from the dictionary so that 
+            the remaining fields fron info can be directly copied into
+            extended_info of CSM schema.
+            """
+            details.pop('status', "")
+            details.pop('enclosure-id', -1)
+            details.pop('name', "")
+            details.pop('health-reason', "")
+            details.pop('health', "")
+            details.pop('health-recommendation', "")
+            details.pop('location', "")
+            """
+            Created other_fields key inside extended_info which will contain the
+            remaining fields.
+            """
+            data['extended_info'].update({'other_details': details})
+            module_type = key.split('_')
+            data.update({'module_type': '%s' %(module_type[1])})
+            data.update({'updated_time': '%s' %(datetime.datetime.utcnow())})
+        except KeyError as e:
+            Log.exception(e)
+        return data
+
     def _convert_to_csm_schema(self, message):
         """ 
         Parsing the alert JSON to create the csm schema
         """
-        msg_body = json.loads(message)
-        data = {'$schema': 'http://json-schema.org/draft-03/schema#', 'id':\
-                'http://json-schema.org/draft-03/schema#', 'title':\
-                'CSM HW Schema', 'type': 'object', 'properties':\
-                {'header': {}, 'hw': {}}}
-        
-        dict = msg_body['message']['sensor_response_type']
-        for values in dict.values():
-            data['properties']['header'] = {'type': 'hw', 'alert_type': '%s'\
-                    %(values.get('alert_type', "")), 'type': 'hw', 'status':\
-                    '%s' %(values.get('info', {}).get('status', '')),\
-                    'resolved': 'no', 'acknowledged': 'no', 'description':\
-                    '%s' %(values.get('info', {}).get('health-reason', '')),\
-                     'location': '%s' %(values.get('info', {}).get('location',\
-                     '')), 'severity': '1', 'recommendation': '%s'\
-                     %(values.get('info', {}).get('health-recommendation', ''))}
-            data['properties']['hw'] = {'vendor': '%s' %(values.get('info', {})\
-                    .get('vendor', '')), 'enclosure_id': '%s'\
-                    %(values.get('info', {}).get('enclosure-id', '')), \
-                    'serial_number': '%s' %(values.get('info', {})\
-                    .get('serial-number', '')), 'part_number': '%s'\
-                    %(values.get('info', {}).get('part-number', ''))}
-        return data
+        if isinstance(message, dict) == False:
+            msg_body = json.loads(message)
+        else:
+            msg_body = message
+        body = msg_body['message']['sensor_response_type']
+        csm_schema = {}
+        for key in body:
+            csm_schema = self._prepare_csm_schema(body, key)
+            csm_schema.update({'created_time': '%s' %\
+                    (msg_body.get('time', ""))})
+        return csm_schema
