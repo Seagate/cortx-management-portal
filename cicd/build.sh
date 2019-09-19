c6cdd8b9884a26b4ac6e93720a66bdec54c63471
Ajay Paratmandali <ajay.paratmandali@seagate.com>
2019-09-19 11:27:13 +0530

    EOS-1908: Integrate csm with pyinstaller - Create rpm for csm having binary code

--- cicd/build.sh
+++ cicd/build.sh
@@ -1,25 +1,40 @@
-BUILD_START_TIME=$(date +%s)
-set -e
+#!/bin/bash
 
+set -e
+BUILD_START_TIME=$(date +%s)
 BASE_DIR=$(realpath "$(dirname $0)/..")
 PROG_NAME=$(basename $0)
 DIST=$(realpath $BASE_DIR/dist)
 GUI_DIR="$BASE_DIR/src/eos/gui"
 API_DIR="$BASE_DIR/src/web"
 
-
 usage() {
-    echo "usage: $PROG_NAME [-v <csm version>] [-b <build no>]" 1>&2;
+    echo """usage: $PROG_NAME [-v <csm version>]
+                            [-b <build no>] [-k <key>]
+                            [-p <product_name>]
+                            [-c <all|backend|frontend>] [-t]""" 1>&2;
     exit 1;
 }
 
-while getopts ":g:v:b:" o; do
+while getopts ":g:v:b:p:k:c:t" o; do
     case "${o}" in
         v)
             VER=${OPTARG}
             ;;
         b)
             BUILD=${OPTARG}
+            ;;
+        p)
+            PRODUCT=${OPTARG}
+            ;;
+        k)
+            KEY=${OPTARG}
+            ;;
+        c)
+            COMPONENT=${OPTARG}
+            ;;
+        t)
+            TEST=true
             ;;
         *)
             usage
@@ -31,113 +46,151 @@ cd $BASE_DIR
 [ -z $"$BUILD" ] && BUILD="$(git rev-parse --short HEAD)" \
         || BUILD="${BUILD}_$(git rev-parse --short HEAD)"
 [ -z "$VER" ] && VER=$(cat $BASE_DIR/VERSION)
+[ -z "$PRODUCT" ] && PRODUCT="eos"
+[ -z "$KEY" ] && KEY="eos@ees@csm@pr0duct"
+[ -z "$COMPONENT" ] && COMPONENT="all"
+[ -z "$TEST" ] && TEST=false
 
-echo "Using VERSION=${VER} BUILD=${BUILD} ..."
-# Build UI code and web code
-echo  BASE_DIR $BASE_DIR
-echo GUI_DIR $GUI_DIR
-echo API_DIR $API_DIR
+echo "Using VERSION=${VER} BUILD=${BUILD} PRODUCT=${PRODUCT} TEST=${TEST}..."
 
+################### COPY FRESH DIR ##############################
 
-# Array of directories to include in package.
-DIRS=($(ls -p | grep "/" | egrep -v "(dist|jenkins|experiments|src)" | cut -f1 -d'/'))
-
-# Remove existing directory and create fresh one to accomodate all packages.
-DIST="$BASE_DIR/dist"
+# Create fresh one to accomodate all packages.
 COPY_START_TIME=$(date +%s)
-mkdir -p $DIST/csm
+DIST="$BASE_DIR/dist"
+mkdir -p $DIST/csm/conf $DIST/tmp
 
-# Copy all directories into a temporary directory.
-echo "Copy files to CSM directory"
-cp -R ${DIRS[*]} $BASE_DIR/__init__.py ${DIST}/csm
-rsync --exclude=gui/ --exclude=node_modules/ -av --progress src ${DIST}/csm
-cp -R ${DIST}/csm/schema ${DIST}/csm/src/
+CONF=$BASE_DIR/src/conf/
+cp -R $CONF/etc $CONF/service $DIST/csm/conf
+cp $BASE_DIR/jenkins/csm.spec $DIST/tmp/
 COPY_END_TIME=$(date +%s)
 
-WEB_BUILD_START_TIME=$(date +%s)
-cd ${DIST}/csm/src/web/
-echo "Running Web Build"
-npm install --production
-npm run build-ts
+################### Backend ##############################
 
-#Delete src folder from web
-echo " Deleting web src and eos/gui directory--" ${DIST}/csm/web/src
-cp -R  ${DIST}/csm/src/web/.env ${DIST}/csm/src/web/web-dist
-rm -rf ${DIST}/csm/src/web/src
-WEB_BUILD_END_TIME=$(date +%s)
-UI_BUILD_START_TIME=$(date +%s)
-echo "Running UI Build"
-cd $GUI_DIR
-npm install
-npm run build
+if [ "$COMPONENT" == "all" ] || [ "$COMPONENT" == "backend" ]; then
+    # Build CSM Backend
+    CORE_BUILD_START_TIME=$(date +%s)
 
-UI_BUILD_END_TIME=$(date +%s)
+    # Copy Backend files
+    mkdir -p $DIST/csm/lib $DIST/csm/bin $DIST/csm/conf $DIST/tmp
+    cp -rs $BASE_DIR/src/ $DIST/tmp/csm
+    cp -rs $BASE_DIR/test/ $DIST/tmp/csm
 
+    # Enable csm package for python import
+    ln -sf $DIST/tmp/csm/  /usr/lib/python3.6/site-packages/
 
+    CONF=$BASE_DIR/src/conf/
+    cp -R $BASE_DIR/schema $DIST/csm/
+
+    # Check python package
+    req_file=$BASE_DIR/jenkins/pyinstaller/requirment.txt
+    for pkg in $(cat $req_file); do
+        echo "Checking for $pkg package"
+        pip3 install $pkg > /dev/null || { echo "Package missing $pkg"; exit 1;};
+    done
+
+    # Create spec for pyinstaller
+    cd $DIST/tmp
+    [ "$TEST" == true ] && {
+        PYINSTALLER_FILE=$DIST/tmp/${PRODUCT}_csm_test.spec
+        cp $BASE_DIR/jenkins/pyinstaller/product_csm_test.spec ${PYINSTALLER_FILE}
+        mkdir -p $DIST/csm/test
+        cp -R $BASE_DIR/test/plans $BASE_DIR/test/args.yaml $DIST/csm/test
+    } || {
+        PYINSTALLER_FILE=$DIST/tmp/${PRODUCT}_csm.spec
+        cp $BASE_DIR/jenkins/pyinstaller/product_csm.spec ${PYINSTALLER_FILE}
+    }
+
+    sed -i -e "s|<PRODUCT>|${PRODUCT}|g" \
+        -e "s|<CSM_PATH>|${DIST}/tmp/csm|g" ${PYINSTALLER_FILE}
+    pyinstaller --clean -y --distpath ${DIST}/csm --key ${KEY} ${PYINSTALLER_FILE}
+    CORE_BUILD_END_TIME=$(date +%s)
+fi
+
+################### WEB & UI ##############################
+
+if [ "$COMPONENT" == "all" ] || [ "$COMPONENT" == "frontend" ]; then
+    WEB_BUILD_START_TIME=$(date +%s)
+
+    # Copy frontend files
+    mkdir -p $DIST/csm/eos/gui/
+    cp -R $BASE_DIR/src/web ${DIST}/csm
+
+    echo "Running Web Build"
+    cd ${DIST}/csm/web/
+    npm install --production
+    npm run build-ts
+
+    #Delete src folder from web
+    echo " Deleting web src and eos/gui directory--" ${DIST}/csm/web/src
+    cp -R  ${DIST}/csm/web/.env ${DIST}/csm/web/web-dist
+    rm -rf ${DIST}/csm/web/src
+    WEB_BUILD_END_TIME=$(date +%s)
+
+    UI_BUILD_START_TIME=$(date +%s)
+    echo "Running UI Build"
+    cd $GUI_DIR
+    npm install
+    npm run build
+
+    UI_BUILD_END_TIME=$(date +%s)
+fi
+
+################### TAR & RPM BUILD ##############################
+
+# Remove existing directory tree and create fresh one.
 TAR_START_TIME=$(date +%s)
 cd $BASE_DIR
-# Remove existing directory tree and create fresh one.
 \rm -rf ${DIST}/rpmbuild
 mkdir -p ${DIST}/rpmbuild/SOURCES
 
+# Genrate spec file for CSM
+sed -i -e "s/<RPM_NAME>/${PRODUCT}_csm/g" \
+    -e "s/<PRODUCT>/${PRODUCT}/g" $DIST/tmp/csm.spec
+
 cd ${DIST}
 # Create tar for csm
-tar -czvf ${DIST}/rpmbuild/SOURCES/csm-${VER}.tar.gz \
-    $(find csm -type f | egrep -v "(test|eos)")
-# Create tar for eos-csm
-tar -czvf ${DIST}/rpmbuild/SOURCES/eos-csm-${VER}.tar.gz csm/src/eos
-# Create tar for csm-test
-tar -czvf rpmbuild/SOURCES/csm-test-${VER}.tar.gz \
-    $(find csm/test -type f | grep -v eos)
-
-# Create tar for eos-csm-test
-tar -czvf rpmbuild/SOURCES/eos-csm-test-${VER}.tar.gz \
-    csm/test/eos
-cd -
+echo "Creating tar for csm build"
+tar -czf ${DIST}/rpmbuild/SOURCES/${PRODUCT}-csm-${VER}.tar.gz csm
 TAR_END_TIME=$(date +%s)
 
-
-# Remove temporary directory
-\rm -rf ${DIST}/csm
-
-RPM_BUILD_START_TIME=$(date +%s)
 # Generate RPMs
+RPM_BUILD_START_TIME=$(date +%s)
 TOPDIR=$(realpath ${DIST}/rpmbuild)
 
 # CSM RPM
 echo rpmbuild --define "version $VER" --define "dist $BUILD" --define "_topdir $TOPDIR" -bb $BASE_DIR/jenkins/csm.spec
-rpmbuild --define "version $VER" --define "dist $BUILD" --define "_topdir $TOPDIR" -bb --with python36 $BASE_DIR/jenkins/csm.spec
-
-# EOS CSM RPM
-echo rpmbuild --define "version $VER" --define "dist $BUILD" --define "_topdir $TOPDIR" -bb $BASE_DIR/jenkins/eos-csm.spec
-rpmbuild --define "version $VER" --define "dist $BUILD" --define "_topdir $TOPDIR" -bb --with python36 $BASE_DIR/jenkins/eos-csm.spec
-
-
-# CSM TEST RPM
-echo rpmbuild --define "version $VER" --define "dist $BUILD" --define "_topdir $TOPDIR" -bb $BASE_DIR/jenkins/csm-test.spec
-rpmbuild --define "version $VER" --define "dist $BUILD" --define "_topdir $TOPDIR" -bb --with python36 $BASE_DIR/jenkins/csm-test.spec
-
-# EOS CSM TEST RPM
-echo rpmbuild --define "version $VER" --define "dist $BUILD" --define "_topdir $TOPDIR" -bb $BASE_DIR/jenkins/eos-csm-test.spec
-rpmbuild --define "version $VER" --define "dist $BUILD" --define "_topdir $TOPDIR" -bb --with python36 $BASE_DIR/jenkins/eos-csm-test.spec
-
-\rm -rf ${DIST}/rpmbuild/BUILDROOT/* ${DIST}/rpmbuild/BUILD/*
-find $BASE_DIR -name *.rpm
+rpmbuild --define "version $VER" --define "dist $BUILD" --define "_topdir $TOPDIR" -bb $DIST/tmp/csm.spec
 RPM_BUILD_END_TIME=$(date +%s)
 
+# Remove temporary directory
+\rm -rf ${DIST}/csm
+\rm -rf $DIST/tmp
+\rm -rf /usr/lib/python3.6/site-packages/csm
 BUILD_END_TIME=$(date +%s)
+
+echo "CSM RPMs ..."
+find $BASE_DIR -name *.rpm
 
 COPY_DIFF=$(( $COPY_END_TIME - $COPY_START_TIME ))
 printf "COPY TIME!!!!!!!!!!!!"
 printf "%02d:%02d:%02d\n" $(( COPY_DIFF / 3600 )) $(( ( COPY_DIFF / 60 ) % 60 )) $(( COPY_DIFF % 60 ))
 
-WEB_DIFF=$(( $WEB_BUILD_END_TIME - $WEB_BUILD_START_TIME ))
-printf "Web Build TIME!!!!!!!!!!!!"
-printf "%02d:%02d:%02d\n" $(( WEB_DIFF / 3600 )) $(( ( WEB_DIFF / 60 ) % 60 )) $(( WEB_DIFF % 60 ))
+if [ "$COMPONENT" == "all" ] || [ "$COMPONENT" == "backend" ]; then
+    CORE_DIFF=$(( $CORE_BUILD_END_TIME - $CORE_BUILD_START_TIME ))
+    printf "CORE BUILD TIME!!!!!!!!!!!!"
+    printf "%02d:%02d:%02d\n" $(( CORE_DIFF / 3600 )) $(( ( CORE_DIFF / 60 ) % 60 )) $(( CORE_DIFF % 60 ))
+fi
 
-UI_DIFF=$(( $UI_BUILD_END_TIME - $UI_BUILD_START_TIME ))
-printf "UI Build time !!!!!!!!!!!!"
-printf "%02d:%02d:%02d\n" $(( UI_DIFF / 3600 )) $(( ( UI_DIFF / 60 ) % 60 )) $(( UI_DIFF % 60 ))
+if [ "$COMPONENT" == "all" ] || [ "$COMPONENT" == "frontend" ]; then
+    WEB_DIFF=$(( $WEB_BUILD_END_TIME - $WEB_BUILD_START_TIME ))
+    printf "Web Build TIME!!!!!!!!!!!!"
+    printf "%02d:%02d:%02d\n" $(( WEB_DIFF / 3600 )) $(( ( WEB_DIFF / 60 ) % 60 )) $(( WEB_DIFF % 60 ))
+
+    UI_DIFF=$(( $UI_BUILD_END_TIME - $UI_BUILD_START_TIME ))
+    printf "UI Build time !!!!!!!!!!!!"
+    printf "%02d:%02d:%02d\n" $(( UI_DIFF / 3600 )) $(( ( UI_DIFF / 60 ) % 60 )) $(( UI_DIFF % 60 ))
+fi
 
 TAR_DIFF=$(( $TAR_END_TIME - $TAR_START_TIME ))
 printf "Time taken in creating TAR !!!!!!!!!!!!"
