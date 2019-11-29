36144c850c4b4bc7dcb3aa471466803ed2bfc653
Soniya Moholkar <soniya.moholkar@seagate.com>
2019-11-29 13:40:17 +0530

    Capacity API intagration with Python
    Websocket code is commented as authorization is not implemented there, server is getting hanged.

--- web/src/services/api-register.ts
+++ web/src/services/api-register.ts
@@ -27,5 +27,6 @@ export default {
 	udx_devices: `/usl/v1/devices`,
     s3_accounts: `/api/${version}/s3_accounts`,
     s3_iam_users: `/api/${version}/iam_users`,
-    s3_bucket: `/api/${version}/s3/bucket`
+    s3_bucket: `/api/${version}/s3/bucket`,
+    capacity: `/api/${version}/capacity`
 };
