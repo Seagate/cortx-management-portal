32d32f77db0af2736632b2b2948e4136e7abc42a
Soniya Moholkar <soniya.moholkar@seagate.com>
2020-01-20 08:28:38 +0000

    EOS:4579- Node API to acknowledge all specified alerts

--- web/src/services/alerts/alerts-controller.ts
+++ web/src/services/alerts/alerts-controller.ts
@@ -34,9 +34,14 @@ export const getAlerts = async (req: Request, res: Response) => {
     return result;
 };
 
+export const updateAlert = async (req: Request, res: Response) => {
+    let alertData = Api.patch(apiRegister.all_alerts, req, res, req.params.alert_id);
+    let result = await alertData;
+    return result;
+};
 
 export const updateAlerts = async (req: Request, res: Response) => {
-    let alertData = Api.patch(apiRegister.all_alerts, req, res, req.params.alert_id);
+    let alertData = Api.patch(apiRegister.all_alerts, req, res, "");
     let result = await alertData;
     return result;
 };
