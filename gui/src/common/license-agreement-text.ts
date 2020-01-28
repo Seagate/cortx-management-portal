b699daeab4624c6d1484491fd42a87b46f387d2c
Vaibhav Bhavsar <vaibhav.bhavsar@seagate.com>
2020-01-28 19:15:18 +0530

    Added license agreement PopUp on welcome

---
+++ gui/src/common/license-agreement-text.ts
@@ -0,0 +1,30 @@
+export const agreementText = {
+  title: `EOS Agreement`,
+  description: `EOS collects and stores machine diagnostic information,
+including but not limited to configuration collects information, user supplied contact
+information, names of data volumes, IP addresses, access control lists, diagnostics &
+performance information, network configuration information, host/ server configuration &
+performance information and related data "Collected Data") and transmits this information
+to Seagate. By checking the box below, you agree to allow Seagate to provide
+remote monitoring services of your Seagate Storage Device and you give Seagate the
+right to collect the Collected Data in accordance with Seagate's privacy policy,
+available in order to enable the performance of all of the various
+functions of the Support Tool during your entitlement to receive related repair services
+from Seagate. You agree that the provision of the Support Tool may involve
+international transfers of data from you to Seagate and/or to Seagate's affiliates,
+subcontractors or business partners. When making such transfers, Seagate shall ensure
+appropriate protection IS in place to safeguard the Collected Data being transferred in
+connection with the Support Tool. Additionally, Seagate shall put in place appropriate
+technical and organizational measures to protect Collected Data against accidental or
+unlawful destruction or accidental loss, alteration, unauthorized disclosure or access.
+If at soma future data you do not wish for Seagate to continue collecting,
+storing and analyzing this machine diagnostic information, you must contact Seagate
+and request that your data be removed from the diagnostics databases. If you are
+enabling this tool on behalf of a corcpany or other legal entity, you are further
+certifying to Seagate that you have appropriate authority to provide this consent on
+behalf of that entity. If you do not consent to the collection, transmission and/or
+use of the Collected Data, the SupportAssist feature will be disabled, and the
+benefits of proactive service requests, real—time troubleshooting, automatic support
+case creation and health checks, Prosupport Plus and Optimize service delivery, and
+remote Storage Center upgrades will not be available . `
+};
---
+++ gui/src/components/preboarding/license-agreement.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <v-container>
+      <div class="eos-modal-container">
+        <div class="eos-modal eos-modal-onboarding">
+          <div class="eos-modal-header">
+            <label> {{ agreementText.title }}</label>
+            <img
+              class="eos-modal-close"
+              :src="require('@/assets/close-green.svg')"
+              @click="cancelAgreement"
+            />
+          </div>
+          <div class="eos-modal-body ">
+            <div class="title-container">
+              {{ agreementText.description }}
+            </div>
+            <div class="mt-8  nav-btn">
+              <button
+                type="button"
+                class="eos-btn-primary mr-2"
+                @click="acceptAgreement"
+              >
+                Accept
+              </button>
+
+              <button
+                type="button"
+                class="eos-btn-tertiary"
+                @click="cancelAgreement"
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </v-container>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+import { agreementText } from "./../../common/license-agreement-text";
+@Component({
+  name: "eos-onboarding-finish"
+})
+export default class EosOnboardingFinish extends Vue {
+  private data() {
+    return {
+      agreementText: agreementText
+    };
+  }
+  private acceptAgreement() {
+    this.$emit("accepted", true);
+  }
+  private cancelAgreement() {
+    this.$emit("accepted", false);
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.eos-modal-onboarding {
+  width: 700px;
+  min-height: 214px;
+  height: 470px;
+  position: fixed;
+}
+.nav-btn {
+  text-align: right;
+}
+.finish-text {
+  vertical-align: center;
+  float: left;
+}
+.success-img {
+  margin-top: 3px;
+  float: left;
+}
+.title-container {
+  height: 300px;
+  overflow: auto;
+}
+</style>
