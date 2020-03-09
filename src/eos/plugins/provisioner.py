177e631623488716fc6dc8347644cd70e2967c65
Udayan Yaragattikar <udayan.yaragattikar@seagate.com>
2020-03-09 19:31:59 +0530

    Addressing comments. Adding Status api for last upgrade

--- src/eos/plugins/provisioner.py
+++ src/eos/plugins/provisioner.py
@@ -1,13 +1,24 @@
 # import provisioner
+from csm.common.log import Log
 
-class ProvisionerPlugin():
+
+class ProvisionerPlugin:
     def __init__(self):
         self._provisioner = "provisioner"
 
     async def validate_package(self, file_path):
-        # TODO: Provisioner api to validate package tobe implented here            
-        return "1.2.3"
-    
+        # TODO: Provisioner api to validate package tobe implented here
+        Log.debug(f"Validating package: f{file_path}")
+        return {"version": "1.2.3",
+                "file_path": file_path}
+
     async def trigger_firmware_upload(self):
         # TODO: Provisioner api to trigger firmware update tobe implented here
         return "Firmware update triggered succesfully"
+
+    async def get_last_firmware_upgrade_status(self):
+        # TODO: Provisioner api to get last firmware upgrade status.
+        Log.debug("Getting last firmware upgrade status")
+        return {"status": "Successful",
+                "DateTime": "YYYY-MM-DD-HH:MM:SS",
+                "version": "1.2.3"}
