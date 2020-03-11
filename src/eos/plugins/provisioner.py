bd1498d0e34c4dae4ae33e46403a4707b5c8ec96
Udayan Yaragattikar <udayan.yaragattikar@seagate.com>
2020-03-11 12:34:15 +0530

    Addressing comments.

--- src/eos/plugins/provisioner.py
+++ src/eos/plugins/provisioner.py
@@ -1,3 +1,22 @@
+#!/usr/bin/env python3
+
+"""
+ ****************************************************************************
+ Filename:          provisioner.py
+ Description:       Contains the implementation of Provisioner plugin.
+
+ Creation Date:     02/25/2020
+ Author:            Udayan Yaragattikar
+
+ Do NOT modify or remove this copyright and confidentiality notice!
+ Copyright (c) 2001 - $Date: 2015/01/14 $ Seagate Technology, LLC.
+ The code contained herein is CONFIDENTIAL to Seagate Technology, LLC.
+ Portions are also trade secret. Any use, duplication, derivation, distribution
+ or disclosure of this code, for any reason, not expressly authorized is
+ prohibited. All other rights are expressly reserved by Seagate Technology, LLC.
+ ****************************************************************************
+"""
+
 # import provisioner
 from csm.common.log import Log
 
