c70ebae79af4b5b0ee97c79a6eb72a45bed0eba8
jayshree more <jayshree.more@seagate.com>
2020-03-26 13:19:11 +0530

    take latest pull from master

--- gui/src/components/onboarding/system-config/ssl-certificate/upload-ssl.vue
+++ gui/src/components/onboarding/system-config/ssl-certificate/upload-ssl.vue
@@ -1,46 +1,17 @@
 <template>
-  <v-container class="mt-0 ml-0">
+  <v-container>
     <div class="pl-4 body-2">
-      <div class="title mt-0 font-weight-bold" id="lblUpdateFirmware">Upload SSL Certificate</div>
-      <div class="mt-6" id="lblUpdateFirmwareMsg">
-        <span class="eos-text-bold">It is important that you are uploading ssl cerificate</span>
-      </div>
-      <div class="mt-6" id="lblVersion"></div>
-      <v-divider class="mt-2 mb-5" />
-      <input
-        type="file"
-        id="file"
-        ref="file"
-        v-on:change="handleFileUpload($event.target.files) " accept =".pem"/>
-      <v-divider class="mt-5 mb-2" />
-      <div class="mt-8">
-        <v-row>
-          <v-col cols="12">
-            <v-row
-              :align="alignment"
-              :justify="justify"
-              class="grey lighten-5"
-              style="height: 300px;"
-            >
-              <button
-                id="btnUploadSSL"
-                type="button"
-                class="eos-btn-primary mt-3"
-                @click="uploadCertificate()"
-              >Upload Certificate</button>
-              <span class="ml-7" v-if="!route">
-                <EOSInstallSSL />
-              </span>
-            </v-row>
-          </v-col>
-        </v-row>
-      </div>
+      <div class="title mt-0 font-weight-bold" id="lblIpv4Dns">SSL Upload</div>
+      <div
+        class="mt-6"
+        id="lblIpv4Msg"
+      >You need to upload ssl and key certificate here</div>
+      <eosUploadSSLConfig/>
     </div>
    </v-container>
 </template>
 <script lang="ts">
 import { Component, Vue, Prop } from "vue-property-decorator";
-import EOSInstallSSL from "./install-ssl.vue";
 import {
   SystemConfigObject,
   DnsNetworkSettings
@@ -59,58 +30,13 @@ import EOSUploadSSLConfig from "./upload-ssl-config.vue";
 @Component({
   name: "uploade-ssl",
   components: {
-    EOSInstallSSL
+    eosUploadSSLConfig: EOSUploadSSLConfig
   }
 })
 
 export default class EOSUploadSSL extends Vue {
-  @Validations()
-  private validations = {
-    file: { required }
-  };
-  private mounted() {
-    if (this.$route.path == "/onboarding") {
-      this.$data.route = true;
-    } else {
-      this.$data.route = false;
-    }
-    this.$data.route == this.$route.name;
-    console.log(this.$data.route == this.$route.name, "component");
-    console.log(this.$route, "$route.name");
-    // WizardHook: Open a listener for onNext event
-    // So when wizard footer clicks on the Next Button this component can perform its own workflow
-    EVENT_BUS.$on("emitOnNext", (res: any) => {
-      res(true);
-    });
-  }
-  private destroyed() {
-    // WizardHook: shut off on exit event listner
-    EVENT_BUS.$off("emitOnNext");
-  }
-  get isValidForm() {
-    const validate = true;
-    // WizardHook: Emit event to sibling wizard footer component
-    // to send information about data validation to enable/disable wizard footer
-    EVENT_BUS.$emit("validForm", validate);
-    return validate;
-  }
-  public data() {
-    return {
-      file: File,
-      route: "false"
-    };
-  }
-  private handleFileUpload(fileList: FileList) {
-    this.$data.file = fileList[0];
-  }
-  private uploadCertificate() {
-    const formData = new FormData();
-    formData.append("pemfile", this.$data.file);
-    this.$store.dispatch("sslupload/uploadSSLCerificate", formData);
-  }
 }
 
 </script>
 <style lang="scss" scoped>
 </style>
-};
