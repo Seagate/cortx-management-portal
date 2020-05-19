b73f8f1399dee51ab9610e682068df854c59dea0
Namrata Khake <namrata.khake@seagate.com>
2020-05-19 15:12:13 +0000

    UI changes-updated licence workflow

--- gui/src/components/preboarding/license.vue
+++ gui/src/components/preboarding/license.vue
@@ -10,61 +10,16 @@
         Welcome to the Storage Management Console.
       </div>
       <div class="ml-4 white--text">
-        To set up your system, you must enter the License key.
+        To set up your system, you must accept the EULA.
       </div>
       <div class="ml-4 mt-1 mb-8 white--text">
         Click Get started to agree
         <span class="csmprimary--text">EULA.</span>
       </div>
-      <v-row class="ml-4 eos-form-group">
-        <v-col class="mt-1" cols="3">
-          <label class="white--text" for="label">License Key*: </label>
-        </v-col>
-        <v-col>
-          <input
-            class="eos-form__input_text"
-            type="text"
-            maxlength="4"
-            v-on:keyup="moveOnNext"
-            v-model="key1"
-          />
-        </v-col>
-        <v-col>
-          <input
-            class="eos-form__input_text"
-            type="text"
-            ref="key2"
-            maxlength="4"
-            v-on:keyup="moveOnNext"
-            v-model="key2"
-          />
-        </v-col>
-        <v-col>
-          <input
-            class="eos-form__input_text"
-            type="text"
-            ref="key3"
-            maxlength="4"
-            v-on:keyup="moveOnNext"
-            v-model="key3"
-          />
-        </v-col>
-        <v-col>
-          <input
-            class="eos-form__input_text"
-            type="text"
-            maxlength="4"
-            ref="key4"
-            v-on:keyup="moveOnNext"
-            v-model="key4"
-          />
-        </v-col>
-      </v-row>
-      <button
+        <button
         type="button"
         class="eos-btn-primary-dark ml-4"
-        @click="addLicense"
-        :disabled="!getStarted"
+        @click="showLicenseAgreement = true"
       >
         <span>Get started</span>
       </button>
@@ -86,38 +41,11 @@ import LicenseAgreement from "./license-agreement.vue";
 export default class EosLicense extends Vue {
   private data() {
     return {
-      key1: "",
-      key2: "",
-      key3: "",
-      key4: "",
       showLicenseAgreement: false,
       getStarted: false
     };
   }
-  private moveOnNext() {
-    if (this.$data.key1.length >= 4) {
-      const inputRef: any = this.$refs.key2;
-      inputRef.focus();
-    }
-    if (this.$data.key2.length >= 4) {
-      const inputRef: any = this.$refs.key3;
-      inputRef.focus();
-    }
-    if (this.$data.key3.length >= 4) {
-      const inputRef: any = this.$refs.key4;
-      inputRef.focus();
-    }
-    if (
-      this.$data.key4.length >= 4 &&
-      this.$data.key3.length >= 4 &&
-      this.$data.key2.length >= 4 &&
-      this.$data.key1.length >= 4
-    ) {
-      this.$data.getStarted = true;
-    } else {
-      this.$data.getStarted = false;
-    }
-  }
+
   private licenseAcceptedOrClosed(status: boolean) {
     if (status) {
       this.gotToNextPage();
@@ -125,27 +53,9 @@ export default class EosLicense extends Vue {
       this.$data.showLicenseAgreement = false;
     }
   }
+
   private gotToNextPage() {
     this.$router.push("adminuser");
-  }
-  private addLicense() {
-    const licensekey = {
-      csm_onboarding_license_key: this.$data.key1.concat(
-        this.$data.key2,
-        this.$data.key3,
-        this.$data.key4
-      )
-    };
-    try {
-      this.$store
-        .dispatch("userLogin/addLicenseKey", licensekey)
-        .then((res: any) => {
-          this.$data.showLicenseAgreement = true;
-        });
-    } catch (e) {
-      // tslint:disable-next-line: no-console
-      return e;
-    }
   }
 }
 </script>
