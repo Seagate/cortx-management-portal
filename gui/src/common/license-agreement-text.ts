10f7fb6162bd9e406b5589bddbe550f57b7d231c
Vaibhav Bhavsar <vaibhav.bhavsar@seagate.com>
2020-06-01 02:45:39 +0000

    [UI: Onboarding] License agreement updated in HTML format

--- gui/src/common/license-agreement-text.ts
+++ gui/src/common/license-agreement-text.ts
@@ -1,30 +1,129 @@
 export const agreementText = {
-  title: `EOS Agreement`,
-  description: `EOS collects and stores machine diagnostic information,
-including but not limited to configuration collects information, user supplied contact
-information, names of data volumes, IP addresses, access control lists, diagnostics &
-performance information, network configuration information, host/ server configuration &
-performance information and related data "Collected Data") and transmits this information
-to Seagate. By checking the box below, you agree to allow Seagate to provide
-remote monitoring services of your Seagate Storage Device and you give Seagate the
-right to collect the Collected Data in accordance with Seagate's privacy policy,
-available in order to enable the performance of all of the various
-functions of the Support Tool during your entitlement to receive related repair services
-from Seagate. You agree that the provision of the Support Tool may involve
-international transfers of data from you to Seagate and/or to Seagate's affiliates,
-subcontractors or business partners. When making such transfers, Seagate shall ensure
-appropriate protection IS in place to safeguard the Collected Data being transferred in
-connection with the Support Tool. Additionally, Seagate shall put in place appropriate
-technical and organizational measures to protect Collected Data against accidental or
-unlawful destruction or accidental loss, alteration, unauthorized disclosure or access.
-If at soma future data you do not wish for Seagate to continue collecting,
-storing and analyzing this machine diagnostic information, you must contact Seagate
-and request that your data be removed from the diagnostics databases. If you are
-enabling this tool on behalf of a corcpany or other legal entity, you are further
-certifying to Seagate that you have appropriate authority to provide this consent on
-behalf of that entity. If you do not consent to the collection, transmission and/or
-use of the Collected Data, the SupportAssist feature will be disabled, and the
-benefits of proactive service requests, real—time troubleshooting, automatic support
-case creation and health checks, Prosupport Plus and Optimize service delivery, and
-remote Storage Center upgrades will not be available . `
+  title: `LyveDrive Agreement`,
+  description: `<div
+  class="font-weight-bold text-center"
+  style="margin: 0 100px;"
+>
+  LYVE DRIVE RACK BETA CYCLE PROGRAM PARTICIPATION AUTHORIZATION AND
+  WAIVER
+</div>
+<table class="mt-5">
+  <tr>
+    <td
+      class="font-weight-bold"
+      style="width: 50px; vertical-align: top;"
+    >
+      I.
+    </td>
+    <td>
+      <div
+        class="font-weight-bold"
+        style="text-decoration: underline;"
+      >
+        Beta Test Program Description
+      </div>
+      <p>
+        LyveDrive RACK (“Product”) collects and stores machine
+        diagnostic information, including but not limited to
+        configuration collects information, user supplied contact
+        information, names of data volumes, IP addresses, access
+        control lists, diagnostics & performance information,
+        network configuration information, host/ server
+        configuration & performance information and related data
+        ("Collected Data") and transmits this information to
+        Seagate. You acknowledge that the Product is a Beta
+        Product still under development, and is not intended for
+        commercial sale or use, and is provided “As-Is”. The Beta
+        Product is subject to change at the sole discretion of
+        Seagate.
+      </p>
+    </td>
+  </tr>
+  <tr>
+    <td
+      class="font-weight-bold"
+      style="width: 50px; vertical-align: top;"
+    >
+      II.
+    </td>
+    <td>
+      <div
+        class="font-weight-bold"
+        style="text-decoration: underline;"
+      >
+        Authorization and Waive
+      </div>
+      <p>
+        By signing below, You agree to allow Seagate to provide
+        remote monitoring services of Your Seagate Storage Device
+        and You give Seagate the right to collect the Collected
+        Data in accordance with Seagate's privacy policy. Your
+        right to use the Seagate Storage Device includes the
+        performance of all of the various functions of the Support
+        Tool and Your entitlement to receive related repair
+        services from Seagate. You agree that the provision of the
+        Support Tool may involve international transfers of data
+        from You to Seagate and/or to Seagate's affiliates,
+        sub-contractors or business partners. When making such
+        transfers, Seagate shall endeavor to provide appropriate
+        protection to safeguard the Collected Data being
+        transferred in connection with the Support Tool.
+        Additionally, Seagate shall endeavor to put in place
+        appropriate technical and organizational measures to
+        protect Collected Data against accidental or unlawful
+        destruction or accidental loss, alteration, unauthorized
+        disclosure or access. However, Seagate may need to
+        reset/update the Product at any time in this Beta stage,
+        which may cause data loss. Beta Product is not yet GDPR
+        compliant and You expressly acknowledge that You
+        understand this data privacy risk and potential loss of
+        data. Seagate does not warrant the safety of such
+        Collected Data and You are expressly waiving Seagate’s
+        liability for data put into Beta Product. If at some
+        future data You do not wish for Seagate to continue
+        collecting, storing and analyzing this machine diagnostic
+        information, You must contact Seagate and request that
+        Your data be removed from the diagnostics databases. If
+        You are enabling this tool on behalf of a company or other
+        legal entity, You are further certifying to Seagate that
+        You have appropriate authority to provide this consent on
+        behalf of that entity. If You do not consent to the
+        collection, transmission and/or use of the Collected Data,
+        the SupportAssist feature will be disabled, and the
+        benefits of proactive service requests, real-time
+        troubleshooting, automatic support case creation and
+        health checks, Prosupport Plus and Optimize service
+        delivery, and remote Storage Center upgrades will not be
+        available.
+      </p>
+      <div>
+        At the end of this Beta Cycle, You agree to:
+      </div>
+      <ul style="list-style-type: square;">
+        <li>
+          Return all Seagate Beta Products, including all hardware
+          after Beta Cycle.
+        </li>
+        <li>
+          Delete all data on Beta Products prior to returning
+          them.
+        </li>
+      </ul>
+    </td>
+  </tr>
+</table>
+
+<p class="mt-5">
+  In consideration of participation of this Beta Cycle Program, I,
+  on behalf of myself and any entity I am authorized to represent,
+  hereby WAIVE, FULLY RELEASE, AND DISCHARGE from any and all
+  liability, claims, demands, damages, costs, expenses, actions,
+  and causes of action as for any data loss, Seagate Technology
+  LLC and its Affiliates, and its Directors, Employees, Officers,
+  Owners, and Agents. I, on behalf of myself and any entity I am
+  authorized to represent, also agree to INDEMNIFY AND HOLD
+  HARMLESS the entities mentioned in this paragraph from any
+  liabilities or claims made by other individuals or entities as a
+  result of my participation in Seagate’s Beta Test Program.
+</p>`
 };
