8254de0643a9f373579d5e885e5d69b0e9ea7517
ajayshingare <66410054+ajayshingare@users.noreply.github.com>
2020-08-24 09:15:17 +0530

    EOS-12055 Updated UI files with copywrite information. (#1054)
    * Updated UI files with new copywrite info
    
    * Updated copywrite info with AGPLv3
    
    Co-authored-by: kupranay <66459168+kupranay@users.noreply.github.com>

--- web/src/services/onboarding/on-boarding-controller.ts
+++ web/src/services/onboarding/on-boarding-controller.ts
@@ -1,19 +1,19 @@
-/*****************************************************************************
- Filename:          on-boarding-controller.ts
- Description:       Gets the data from provider and process it and send back
-                    to client.
-
- Creation Date:     21/08/2019
- Author:            Soniya Moholkar
-
- Do NOT modify or remove this copyright and confidentiality notice!
- Copyright (c) 2001 - $Date: 2015/01/14 $ Seagate Technology, LLC.
- The code contained herein is CONFIDENTIAL to Seagate Technology, LLC.
- Portions are also trade secret. Any use, duplication, derivation, distribution
- or disclosure of this code, for any reason, not expressly authorized is
- prohibited. All other rights are expressly reserved by Seagate Technology, LLC.
- *****************************************************************************/
-
+/*
+* CORTX-CSM: CORTX Management web and CLI interface.
+* Copyright (c) 2020 Seagate Technology LLC and/or its Affiliates
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published
+* by the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Affero General Public License for more details.
+* You should have received a copy of the GNU Affero General Public License
+* along with this program. If not, see <https://www.gnu.org/licenses/>.
+* For any questions about this software or licensing,
+* please email opensource@seagate.com or cortx-questions@seagate.com.
+*/
 import { Api } from "../api";
 import apiRegister from "../api-register";
 import { Request, Response, request, response } from "express";
