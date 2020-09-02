5bbe2a8c3fa59ee9020f66a4c0f2602825afc293
Angvish <kumar.angvish@seagate.com>
2020-09-02 10:56:42 +0530

    Update build.sh

--- cicd/build.sh
+++ cicd/build.sh
@@ -13,7 +13,7 @@
 # For any questions about this software or licensing,
 # please email opensource@seagate.com or cortx-questions@seagate.com.
 
-set -e
+set -x
 BUILD_START_TIME=$(date +%s)
 BASE_DIR=$(realpath "$(dirname $0)/..")
 PROG_NAME=$(basename $0)
