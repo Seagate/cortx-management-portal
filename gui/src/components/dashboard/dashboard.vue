fb046b473b8c888795e2e63c1a3efbdf73c59a32
Vaibhav Bhavsar <vaibhav.bhavsar@seagate.com>
2020-09-17 11:41:43 +0530

    Increased height of alert container datatable Fixed dashboard styling and typos
    Signed-off-by: Vaibhav Bhavsar <vaibhav.bhavsar@seagate.com>

--- gui/src/components/dashboard/dashboard.vue
+++ gui/src/components/dashboard/dashboard.vue
@@ -15,17 +15,17 @@
 * please email opensource@seagate.com or cortx-questions@seagate.com.
 */
 <template>
-  <div class="cortx-p-2">
-    <v-row style="border-bottom: 2px solid rgba(0, 0, 0, 0.12);">
+  <div class="cortx-p-1" id="dashboardContainer">
+    <v-row id="statsRowContainer" class="row-container">
       <v-col class="pt-0" cols="12">
         <cortx-stats-medium />
       </v-col>
     </v-row>
-    <v-row>
-      <v-col cols="4" style="border-right: 2px solid rgba(0, 0, 0, 0.12);">
-        <cortx-capacity-guage />
+    <v-row id="alertCapacityRowContainer" class="row-container">
+      <v-col cols="4" class="capacity-gauge-container">
+        <cortx-capacity-gauge />
       </v-col>
-      <v-col cols="8">
+      <v-col cols="8" class="pa-3 pb-0">
         <cortx-alert-medium />
       </v-col>
     </v-row>
@@ -36,7 +36,7 @@
 import { Component, Vue } from "vue-property-decorator";
 import CortxAlertMedium from "./../alerts/alert-medium.vue";
 import CortxStatsMedium from "./stats/stats-medium.vue";
-import CortxCapacityGuage from "./capacity-gauge.vue";
+import CortxCapacityGauge from "./capacity-gauge.vue";
 import * as c3 from "c3";
 
 @Component({
@@ -44,7 +44,7 @@ import * as c3 from "c3";
   components: {
     cortxAlertMedium: CortxAlertMedium,
     cortxStatsMedium: CortxStatsMedium,
-    cortxCapacityGuage: CortxCapacityGuage
+    cortxCapacityGauge: CortxCapacityGauge
   }
 })
 export default class Dashboard extends Vue {}
@@ -52,5 +52,11 @@ export default class Dashboard extends Vue {}
 <style lang="scss" scoped>
 .page {
   position: relative;
+}
+.row-container {
+  border-bottom: 2px solid rgba(0, 0, 0, 0.12);
+}
+.capacity-gauge-container {
+  border-right: 2px solid rgba(0, 0, 0, 0.12);
 }
 </style>
