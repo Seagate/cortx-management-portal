e49f10b60ea67df188cded5bd484cd4e573bf038
Shri Bhargav Metta <shri.metta@seagate.com>
2020-10-02 05:55:22 -0600

    Implemented resizing.
    Signed-off-by: Shri Bhargav Metta <shri.metta@seagate.com>

--- gui/src/components/alerts/alert-medium.vue
+++ gui/src/components/alerts/alert-medium.vue
@@ -138,6 +138,7 @@ import { Component, Vue, Prop, Mixins } from "vue-property-decorator";
 import AlertsMixin from "./../../mixins/alerts";
 import CortxHealthSummary from "../system/health-summary.vue";
 import { alertTblDescriptionDirective } from "./alert-description-directive";
+import { EVENT_BUS } from "../../main";
 
 @Component({
   name: "cortx-alert-medium",
@@ -151,8 +152,14 @@ export default class CortxAlertMedium extends Mixins(AlertsMixin) {
 
   public tableHeight: string = "250";
 
+  public created() {
+    EVENT_BUS.$on("windowResized", (parentHeight: number) => {
+      this.calculateTableHeight(parentHeight);
+    });
+  }
+
   public async mounted() {
-    this.calculateTableHeight();
+    this.calculateTableHeight(this.parentHeight);
 
     if (this.alertPageFilter !== "new") {
       this.alertPageFilter = "new";
@@ -186,15 +193,28 @@ export default class CortxAlertMedium extends Mixins(AlertsMixin) {
     await this.onSortPaginate();
   }
 
-  public calculateTableHeight() {
-    if (this.parentHeight) {
+  public destroyed() {
+    EVENT_BUS.$off("windowResized");
+  }
+
+  public calculateTableHeight(parentHeight: number) {
+    if (parentHeight) {
       /**
        * Need to subtract health summary component height
        * and title container height from parent height i.e 74px
        * to get exact height of the alert table.
        */
-      const calcHeight: number = this.parentHeight - 74;
-      this.tableHeight = calcHeight.toString();
+      const calcHeight: number = parentHeight - 74;
+
+      /**
+       * If calculated height is less than 250px
+       * then set table height to 250px.
+       */
+      if (calcHeight < 250) {
+        this.tableHeight = "250";
+      } else {
+        this.tableHeight = calcHeight.toString();
+      }
     }
   }
 
