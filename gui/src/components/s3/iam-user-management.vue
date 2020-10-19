17caef2229fd2beb646d8d2099d58f97166d8147
Namrata Khake <namrata.khake@seagate.com>
2020-10-19 10:56:46 +0530

    add space after comma for s3url
    Signed-off-by: Namrata Khake <namrata.khake@seagate.com>

--- gui/src/components/s3/iam-user-management.vue
+++ gui/src/components/s3/iam-user-management.vue
@@ -324,7 +324,7 @@
             <td class="py-2 cortx-text-bold credentials-item-label">
               {{ $t("s3.account.s3-url") }}
             </td>
-            <td class="py-2">{{ s3Url.toString() }}</td>
+            <td class="py-2">{{ s3Url[0] }}, {{s3Url[1]}}</td>
           </tr>
           <tr id="iamARN">
             <td class="py-2 cortx-text-bold credentials-item-label">{{ $t("s3.access-key.table-headers.arn") }}</td>
