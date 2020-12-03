922ee720c7413a855c28302cdd7e6bc367ecdb05
Vrishali-web-dev <71690421+Vrishali-web-dev@users.noreply.github.com>
2020-12-03 16:10:59 +0530

    UDX files to merge in main (#234)
    * UDX files to merge in main
    
    * en json file added

--- gui/src/common/regex-helpers.ts
+++ gui/src/common/regex-helpers.ts
@@ -25,7 +25,7 @@ export const accountNameTooltipMessage =
 
 export const usernameTooltipMessage =
   // tslint:disable-next-line
-  "The username must be of minimum 4 characters and maximum 56 characters. The username must be alphanumeric and can contain underscore (_) and dash (-). The username must not be 'root' or 'Root'";
+  "The username must be of minimum 4 characters and maximum 56 characters. The username must be alphanumeric and can contain underscore (_) and dash (-). The username must not be 'root' or 'Root'.";
 
 export const bucketNameRegex = helpers.regex(
   "bucketNameRegex",
@@ -55,8 +55,6 @@ export const udxBucketNameRegex = helpers.regex(
   /^[a-z0-9-]{0,51}[a-z0-9]$/
 );
 // tslint:disable-next-line
-export const udxBucketNameTooltipMessage = `To identify the S3 bucket correctly, the Bucket name will always use "udx" as its prefix. The bucket name must be of minimum 5 characters and maximum 56 characters.
-  Only lowercase, numbers, and dash (-) are allowed. The bucket name cannot start and end with a dash (-).`;
 
 export const udxURLRegex = helpers.regex(
   "udxURLRegex",
--- gui/src/services/api-register.ts
+++ gui/src/services/api-register.ts
@@ -65,7 +65,8 @@ export default {
   node_health: `/api/${version}/system/health/node`,
   version: `/api/${version}/product_version`,
   health_components: `api/${version}/system/health/components`,
-  health_resources: `api/${version}/system/health/resources`,
   system_status: `/api/${version}/system/status`,
-  appliance_info:`/api/${version}/appliance_info`
+  health_resources: `api/${version}/system/health/resources`,
+  appliance_info: `/api/${version}/appliance_info`,
+  udx_saas: `/api/${version}/udx_saas`
 };
