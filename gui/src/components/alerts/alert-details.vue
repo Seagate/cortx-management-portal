b2f54abea25535d78f1d18de13cb0950c0c606e5
Jayshree More <jayshree.more@seagate.com>
2021-04-01 13:01:18 +0530

    added hostname
    Signed-off-by: Jayshree More <jayshree.more@seagate.com>

--- gui/src/components/alerts/alert-details.vue
+++ gui/src/components/alerts/alert-details.vue
@@ -43,7 +43,7 @@
           <label>{{ $t("alerts.cluster") }} {{ alertExtendedInfo.cluster_id }}</label>
           <label>&nbsp;| {{ $t("alerts.site") }} {{ alertExtendedInfo.site_id }}</label>
           <label>&nbsp;| {{ $t("alerts.rack") }} {{ alertExtendedInfo.rack_id }}</label>
-          <label>&nbsp;| {{ $t("alerts.host_name") }} :{{ alert.host_id}}</label>
+          <label>&nbsp;| {{ $t("alerts.host_name") }} :{{ alert.host_name}}</label>
         </div>
         <div>
           <label>{{ $t("alerts.resourceType") }}: {{ alertExtendedInfo.resource_type }}</label>
