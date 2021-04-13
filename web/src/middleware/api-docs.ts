2557ff8744bca4f3c74dde3a3d9a5414d96fc48f
Naresh Gopalakrishnan <52106625+NareshGopal@users.noreply.github.com>
2021-04-13 17:13:14 +0530

    Included options parameter in swaggerUi.setup (#322)
    * Included options parameter in swaggerUi.setup
    
    Signed-off-by: nareshgopal <naresh.gopalakrishnan@seagate.com>
    
    * Made /api/v2 as the default value in Swagger doc
    
    Signed-off-by: nareshgopal <naresh.gopalakrishnan@seagate.com>

--- web/src/middleware/api-docs.ts
+++ web/src/middleware/api-docs.ts
@@ -24,6 +24,6 @@ export const handleAPIDocs = (router: Router) => {
     customCssUrl: '/public/custom.css',
     customJs: '/public/custom.js'
   };
-  router.use("/api-docs", swaggerUi.serveFiles(swaggerDocument, options), swaggerUi.setup(swaggerDocument));
-  router.use("/client-api-docs", swaggerUi.serveFiles(clientSwaggerDocument, options), swaggerUi.setup(clientSwaggerDocument));
+  router.use("/api-docs", swaggerUi.serveFiles(swaggerDocument, options), swaggerUi.setup(swaggerDocument, options));
+  router.use("/client-api-docs", swaggerUi.serveFiles(clientSwaggerDocument, options), swaggerUi.setup(clientSwaggerDocument, options));
 }
