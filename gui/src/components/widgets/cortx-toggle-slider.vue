70a210ce1a0c9194103bba7fc1145daa7b581f60
Naresh Gopalakrishnan <naresh.gopalakrishnan@seagate.com>
2021-06-29 15:04:12 +0530

    Removed unused imports from cortx-toggle-slider
    Signed-off-by: Naresh Gopalakrishnan <naresh.gopalakrishnan@seagate.com>

--- gui/src/components/widgets/cortx-toggle-slider.vue
+++ gui/src/components/widgets/cortx-toggle-slider.vue
@@ -38,7 +38,7 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue, Prop, Watch } from "vue-property-decorator";
+import { Component, Vue, Prop } from "vue-property-decorator";
 
 @Component({
   name: "cortx-toggle-slider",
