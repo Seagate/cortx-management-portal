af2fa784829ac0fe29e88c0d3bac9654b6d3410d
Soniya Moholkar <soniya.moholkar@seagate.com>
2021-08-05 15:11:11 +0000

    production imports
    Signed-off-by: Soniya Moholkar <soniya.moholkar@seagate.com>

--- web/conf/csm_web_setup.py
+++ web/conf/csm_web_setup.py
@@ -229,8 +229,8 @@ def main(argv: dict):
 if __name__ == '__main__':
     sys.path.append(os.path.join(os.path.dirname(pathlib.Path(__file__)), '..', '..', '..'))
     sys.path.append(os.path.join(os.path.dirname(pathlib.Path(os.path.realpath(__file__))), '..', '..'))
-    from csm_web import CSMWeb
-    from csm_web import CSMWebSetupError
-    #from csm.web.conf.csm_web import CSMWeb
-    #from csm.web.conf.csm_web import CSMWebSetupError
+    #from csm_web import CSMWeb
+    #from csm_web import CSMWebSetupError
+    from csm.web.conf.csm_web import CSMWeb
+    from csm.web.conf.csm_web import CSMWebSetupError
     sys.exit(main(sys.argv))
