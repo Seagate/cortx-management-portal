277257ffbc606386aac10b941d39b48a716187ba
Soniya Moholkar <soniya.moholkar@seagate.com>
2021-08-09 13:14:33 +0000

    Addressed review comments.
    Signed-off-by: Soniya Moholkar <soniya.moholkar@seagate.com>

--- web/conf/csm_web.py
+++ web/conf/csm_web.py
@@ -109,6 +109,7 @@ class CSMWeb:
         if os.environ.get("CLI_SETUP") == "true":
             CSMWeb._run_cmd(f"cli_setup prepare --config {self.conf_url}")
         self._prepare_and_validate_confstore_keys("prepare")
+        self._get_cluster_id()
         self._set_deployment_mode()
         self._set_service_user()
         self._set_password_to_csm_user()
@@ -124,6 +125,7 @@ class CSMWeb:
         if os.environ.get("CLI_SETUP") == "true":
             CSMWeb._run_cmd(f"cli_setup config --config {self.conf_url}")
         self._prepare_and_validate_confstore_keys("config")
+        self._get_cluster_id()
         self._set_deployment_mode()
         self._configure_csm_web_keys()
         Log.info("Config complete")
@@ -250,6 +252,14 @@ class CSMWeb:
         """
         Log.info("Setting service user")
         self._user = Conf.get(self.CONSUMER_INDEX, self.conf_store_keys["csm_user_key"])
+        
+    def _get_cluster_id(self):
+        """
+        This Method will get the cluster ID and set to self._cluster_id
+        :return:
+        """
+        Log.info("Setting cluster_id")
+        self._cluster_id = Conf.get(self.CONSUMER_INDEX, self.conf_store_keys["cluster_id"])
 
     def _config_user(self):
         """
@@ -326,9 +336,8 @@ class CSMWeb:
         if decrypt and csm_user_pass:
             Log.info("Decrypting CSM Password.")
             try:
-                cluster_id = Conf.get(self.CONSUMER_INDEX, self.conf_store_keys["cluster_id"])
                 password_decryption_key = self.conf_store_keys["secret_key"].split('>')[0]
-                cipher_key = Cipher.generate_key(cluster_id, password_decryption_key)                
+                cipher_key = Cipher.generate_key(self._cluster_id, password_decryption_key)                
             except KvError as error:
                 Log.error(f"Failed to Fetch Cluster Id. {error}")
                 return None
@@ -355,18 +364,17 @@ class CSMWeb:
         """Setting up password to service user"""
         Log.info("Setting up password to service user")
         if not self._is_user_exist():
-            raise CSMWebSetupError(f"{self._user} not created on system.")
+            raise CSMWebSetupError(rc=-1, message=f"{self._user} not created on system.")
         Log.info("Fetch decrypted password.")
         _password = self._fetch_csm_user_password(decrypt=True)
         if not _password:
             Log.error("Service User Password Not Available.")
-            raise CSMWebSetupError("Service Usergi Password Not Available.")
+            raise CSMWebSetupError(rc=-1, message="Service Usergi Password Not Available.")
         _password = crypt.crypt(_password, "22")
         self._run_cmd(f"usermod -p {_password} {self._user}")
 
     def _fetch_management_ip(self):
-        cluster_id = Conf.get(self.CONSUMER_INDEX, self.conf_store_keys["cluster_id"])
-        virtual_host_key = f"cluster>{cluster_id}>network>management>virtual_host"
+        virtual_host_key = f"cluster>{self._cluster_id}>network>management>virtual_host"
         self._validate_conf_store_keys(self.CONSUMER_INDEX,[virtual_host_key])
         virtual_host = Conf.get(self.CONSUMER_INDEX, virtual_host_key)
         if not virtual_host and not self._is_env_dev:
@@ -376,8 +384,7 @@ class CSMWeb:
         return virtual_host
     
     def _fetch_key_value(self, key: str, default_value: any):
-        cluster_id = Conf.get(self.CONSUMER_INDEX, self.conf_store_keys["cluster_id"])
-        key = f"cluster>{cluster_id}>network>management>{key}"
+        key = f"cluster>{self._cluster_id}>network>management>{key}"
         value = default_value
         try:
             self._validate_conf_store_keys(self.CONSUMER_INDEX,[key])
