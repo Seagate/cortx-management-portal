88f9d07b5cf7f0fb490f54a209b0fd8181c35b63
Naresh Gopalakrishnan <52106625+NareshGopal@users.noreply.github.com>
2021-12-22 15:11:17 +0530

    EOS-26759: Implementing health and alert widget as per the new design (#560)
    * Added grid structure to dashboard and name changes
    
    * changes in main page and fixes for capacity gauge
    
    * Capacity widget - WIP
    
    Signed-off-by: Naresh Gopalakrishnan <naresh.gopalakrishnan@seagate.com>
    
    * Completed the capacity widget as per the new design and modified dashboard view
    
    Signed-off-by: Naresh Gopalakrishnan <naresh.gopalakrishnan@seagate.com>
    
    * Declared a module for json files in type declaration file
    
    Signed-off-by: Naresh Gopalakrishnan <naresh.gopalakrishnan@seagate.com>
    
    * Removed i18n import from capacity gauge file
    
    Signed-off-by: Naresh Gopalakrishnan <naresh.gopalakrishnan@seagate.com>
    
    * Built cards inside health and alerts widget
    
    Signed-off-by: Naresh Gopalakrishnan <naresh.gopalakrishnan@seagate.com>
    
    * Modified card implementation in health and alert widgets
    
    Signed-off-by: Naresh Gopalakrishnan <naresh.gopalakrishnan@seagate.com>
    
    * Implemented alert count logic for alert widget + CSS changes to dashboard and info-card
    
    Signed-off-by: Naresh Gopalakrishnan <naresh.gopalakrishnan@seagate.com>
    
    * Modified info cards to take imageUrl as props
    
    Signed-off-by: Naresh Gopalakrishnan <naresh.gopalakrishnan@seagate.com>
    
    * Dashboard will be aligned to the left in big screens
    
    Signed-off-by: Naresh Gopalakrishnan <naresh.gopalakrishnan@seagate.com>
    
    Co-authored-by: Sujit Singh <sujit.h.singh@seagate.com>
    Co-authored-by: ajaysrivas <66410865+ajaysrivas@users.noreply.github.com>
    Signed-off-by: Sujit Singh <sujit.h.singh@seagate.com>

--- gui/src/components/dashboard/cortx-dashboard-alert-card.vue
+++ gui/src/components/dashboard/cortx-dashboard-alert-card.vue
@@ -1,31 +1,121 @@
-/*
-* CORTX-CSM: CORTX Management web and CLI interface.
-* Copyright (c) 2020 Seagate Technology LLC and/or its Affiliates
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Affero General Public License as published
-* by the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU Affero General Public License for more details.
-* You should have received a copy of the GNU Affero General Public License
-* along with this program. If not, see <https://www.gnu.org/licenses/>.
-* For any questions about this software or licensing,
-* please email opensource@seagate.com or cortx-questions@seagate.com.
-*/
+/* * CORTX-CSM: CORTX Management web and CLI interface. * Copyright (c) 2020
+Seagate Technology LLC and/or its Affiliates * This program is free software:
+you can redistribute it and/or modify * it under the terms of the GNU Affero
+General Public License as published * by the Free Software Foundation, either
+version 3 of the License, or * (at your option) any later version. * This
+program is distributed in the hope that it will be useful, * but WITHOUT ANY
+WARRANTY; without even the implied warranty of * MERCHANTABILITY or FITNESS FOR
+A PARTICULAR PURPOSE. See the * GNU Affero General Public License for more
+details. * You should have received a copy of the GNU Affero General Public
+License * along with this program. If not, see <https://www.gnu.org/licenses/>.
+* For any questions about this software or licensing, * please email
+opensource@seagate.com or cortx-questions@seagate.com. */
 <template>
-  <div>
-    Dashboard alert
+  <div class="alert-widget-container">
+    <p class="cortx-text-lg cortx-text-bold">Alerts</p>
+    <div class="alert-cards-container">
+      <template v-for="(cardDetail, index) in alertCardDetails">
+        <cortx-dashboard-info-card
+          :title="cardDetail.title"
+          :description="cardDetail.description"
+          :imgUrl="cardDetail.imgUrl"
+          :navPath="cardDetail.navPath"
+          :callBack="infoCardCallBack"
+          :key="index"
+        />
+      </template>
+    </div>
   </div>
 </template>
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
+import { Component, Vue, Mixins } from "vue-property-decorator";
+import CortxDashboardInfoCard from "./cortx-dashboard-info-card.vue";
+import AlertsMixin from "../../mixins/alerts";
 
 @Component({
   name: "cortx-dashboard-alert-card",
-  components: {}
+  components: { CortxDashboardInfoCard }
 })
-export default class CortxDashboardAlertCard extends Vue {}
+export default class CortxDashboardAlertCard extends Mixins(AlertsMixin) {
+  public fatalCount = 0;
+  public criticalCount = 0;
+  public errorCount = 0;
+  public warningCount = 0;
+  public informationalCount = 0;
+
+  public async mounted() {
+    await this.onSortPaginate();
+    this.fatalCount = this.alertObject.alerts.filter(
+      alert => alert.severity === "alert"
+    ).length;
+    this.criticalCount = this.alertObject.alerts.filter(
+      alert => alert.severity === "critical"
+    ).length;
+    this.errorCount = this.alertObject.alerts.filter(
+      alert => alert.severity === "error"
+    ).length;
+    this.warningCount = this.alertObject.alerts.filter(
+      alert => alert.severity === "warning"
+    ).length;
+    this.informationalCount = this.alertObject.alerts.filter(
+      alert => alert.severity === "informational"
+    ).length;
+  }
+
+  get alertCardDetails() {
+    return [
+      {
+        title: this.fatalCount,
+        description: "Fatal",
+        imgUrl: "dashboard/alert/fatal-alert.svg",
+        navPath: "/alerts"
+      },
+      {
+        title: this.criticalCount,
+        description: "Critical",
+        imgUrl: "dashboard/alert/fatal-alert.svg",
+        navPath: "/alerts"
+      },
+      {
+        title: this.errorCount,
+        description: "Error",
+        imgUrl: "dashboard/alert/error-alert.svg",
+        navPath: "/alerts"
+      },
+      {
+        title: this.warningCount,
+        description: "Warning",
+        imgUrl: "dashboard/alert/warning-alert.svg",
+        navPath: "/alerts"
+      },
+      {
+        title: this.informationalCount,
+        description: "Informational",
+        imgUrl: "dashboard/alert/informational-alert.svg",
+        navPath: "/alerts"
+      }
+    ];
+  }
+
+  infoCardCallBack(routePath: string) {
+    this.$router.push(routePath);
+  }
+}
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.alert-cards-container {
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+}
+.alert-cards-container > * {
+  width: 48%;
+  margin-bottom: 1em;
+}
+.alert-cards-container > *:last-child {
+  width: 100%;
+}
+.alert-cards-container > *:not(:last-child) {
+  justify-content: flex-start;
+}
+</style>
---
+++ gui/src/components/dashboard/cortx-dashboard-info-card.vue
@@ -0,0 +1,60 @@
+/* * CORTX-CSM: CORTX Management web and CLI interface. * Copyright (c) 2020
+Seagate Technology LLC and/or its Affiliates * This program is free software:
+you can redistribute it and/or modify * it under the terms of the GNU Affero
+General Public License as published * by the Free Software Foundation, either
+version 3 of the License, or * (at your option) any later version. * This
+program is distributed in the hope that it will be useful, * but WITHOUT ANY
+WARRANTY; without even the implied warranty of * MERCHANTABILITY or FITNESS FOR
+A PARTICULAR PURPOSE. See the * GNU Affero General Public License for more
+details. * You should have received a copy of the GNU Affero General Public
+License * along with this program. If not, see <https://www.gnu.org/licenses/>.
+* For any questions about this software or licensing, * please email
+opensource@seagate.com or cortx-questions@seagate.com. */
+<template>
+  <div class="info-card-container" @click="callBack(navPath)">
+    <img :src="require(`@/assets/${imgUrl}`)" alt="" />
+    <div class="card-info">
+      <span class="title">{{ title }}</span>
+      <span class="description">{{ description }}</span>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+
+@Component({
+  name: "cortx-dashboard-info-card",
+  components: {}
+})
+export default class CortxDashboardInfoCard extends Vue {
+  @Prop({ required: true }) public title: string;
+  @Prop({ required: true }) public description: string;
+  @Prop({ required: true }) public navPath: string;
+  @Prop({ required: true }) public imgUrl: string;
+  @Prop({ required: true }) public callBack: Function;
+}
+</script>
+
+<style lang="scss" scoped>
+.info-card-container {
+  padding: 0.75em;
+  border: 1px solid #e5e5e5;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 4px;
+  cursor: pointer;
+  box-shadow: none;
+  transition: box-shadow 200ms ease-in-out;
+}
+.info-card-container:hover {
+  box-shadow: 0px 6px 15px #e5e5e5;
+}
+.card-info {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-left: 1em;
+}
+</style>
