2a4d130eedb685df8aa5c117696b104460824bda
Naresh Gopalakrishnan <naresh.gopalakrishnan@seagate.com>
2022-02-21 11:09:38 +0530

    Configured Vuetify plugin as in the cortx
    Signed-off-by: Naresh Gopalakrishnan <naresh.gopalakrishnan@seagate.com>

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -19,4 +19,27 @@ import Vuetify from "vuetify/lib/framework";
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+    icons: {
+        iconfont: "mdi",
+        },
+        theme: {
+        options: {
+            customProperties: true
+        },
+        themes: {
+            light: {
+                csmprimary: "#6EBE49",
+                csmdisabled: "#B7B7B7",
+                csmborder: "#9E9E9E",
+                csmheader: "#E8E8E8",
+                csmtooltip: "#454545",
+                csmerror: "#DC1F2E",
+                csmwarning: "#F7A528",
+                csmsuccess: "#6EBE49",
+                csminfo: "#00A1DD",
+                csmoverlay: "#9E9E9E",
+            },
+        },
+    }
+});
