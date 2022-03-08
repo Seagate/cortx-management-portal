8069a840b5fed2d7987f978a98a128eb83dd7684
Sujit Singh <sujit.h.singh@seagate.com>
2022-03-08 11:37:01 +0530

    navigation drawer made generic
    Signed-off-by: Sujit Singh <sujit.h.singh@seagate.com>

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,12 @@
     <LrHeader @menu-click="drawer = !drawer" />
     <main>
       <nav class="side-nav">
-        <LrNavigationDrawer :drawer.sync="drawer" />
+        <SgtNavigationDrawer 
+        :drawer.sync="drawer" 
+        :navItems="navItems"
+        brandName="Seagate"
+        brandLogo="images/seagate-green.svg"
+        />
       </nav>
       <div class="main-content">
         <SgtBreadCrumb
@@ -35,7 +40,7 @@
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import LrHeader from "./components/LrHeader.vue";
-import LrNavigationDrawer from "./components/LrNavigationDrawer.vue";
+import SgtNavigationDrawer from "./components/shared/SgtNavigationDrawer/SgtNavigationDrawer.vue";
 import LrRouteComponents from "./components/LrRouteComponents.vue";
 import SgtBreadCrumb from "./components/shared/bread-crumb/SgtBreadCrumb.vue";
 import { basePathList } from "./components/BreadCrumb.constant";
@@ -44,14 +49,57 @@ import { basePathList } from "./components/BreadCrumb.constant";
   name: "App",
   components: {
     LrHeader,
-    LrNavigationDrawer,
+    SgtNavigationDrawer,
     SgtBreadCrumb,
     LrRouteComponents,
   },
 })
 export default class App extends Vue {
   drawer = true;
-
+  navItems = [
+    {
+      title: "dashboard",
+      path: "/dashboard",
+      iconDefault: "images/dashboard-grey.svg",
+      iconActive: "images/dashboard-white.svg",
+      requiredAccess: "alerts",
+    },
+    {
+      title: "health",
+      path: "/health",
+      iconDefault: "images/health-grey.svg",
+      iconActive: "images/health-white.svg",
+      requiredAccess: "sysconfig",
+    },
+    {
+      title: "manage",
+      path: "/manage",
+      iconDefault: "images/manage-grey.svg",
+      iconActive: "images/manage-white.svg",
+      requiredAccess: "s3accounts",
+    },
+    {
+      title: "lyvePilot",
+      path: "/ldp",
+      iconDefault: "images/udx-grey.svg",
+      iconActive: "images/udx-white.svg",
+      requiredAccess: "lyve_pilot",
+    },
+    {
+      title: "settings",
+      path: "/settings",
+      iconDefault: "images/settings-grey.svg",
+      iconActive: "images/settings-white.svg",
+      requiredAccess: "maintenance",
+    },
+    {
+      title: "maintenance",
+      path: "/maintenance",
+      iconDefault: "images/maintenance-grey.svg",
+      iconActive: "images/maintenance-white.svg",
+      requiredAccess: "sysconfig",
+    },
+  ];
   get basePathList() {
     return basePathList;
   }
