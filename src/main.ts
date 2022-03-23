badc6dd11c9def914c0bc81d43ede0929da1f9ad
Naresh Gopalakrishnan <naresh.gopalakrishnan@seagate.com>
2022-03-23 13:05:51 +0530

    Removed the vuelidate package and used Vuetify's implementation
    Signed-off-by: Naresh Gopalakrishnan <naresh.gopalakrishnan@seagate.com>

--- src/main.ts
+++ src/main.ts
@@ -21,10 +21,8 @@ import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 import i18n from "./i18n";
-import Vuelidate from "vuelidate";
 
 Vue.config.productionTip = false;
-Vue.use(Vuelidate);
 
 new Vue({
   router,
